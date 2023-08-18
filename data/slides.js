a960911eeea70c71afe0205310395de2f0a8034b
Jacek <jacek_kozakowski_dev@wp.pl>
2023-08-18 15:50:17 +0200

    Start making slider
    	modified:   index.html
    	modified:   scripts/index.js
    	modified:   styles/style.css
    
    	new file:   images/example.jpg
    	modified:   index.html
    	modified:   scripts/index.js
    	modified:   styles/style.css
    
    	new file:   data/slides.js
    	new file:   images/code.jpg
    	new file:   images/example.jpg
    	modified:   index.html
    	modified:   scripts/index.js
    	modified:   styles/style.css

---
+++ data/slides.js
@@ -0,0 +1,9 @@
+export const slides = [{
+    text: 'Tu może być Twoja strona.',
+    link: 'index.html',
+    image: 'example.jpg'
+}, {
+    text: 'Tu może być Twoja strona.',
+    link: 'index.html',
+    image: 'welcome_image.jpg'
+}];
---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+import {slides} from '../data/slides.js';
+
+let index = 0;
+
+displaySlide(slides[index]);
+
+function displaySlide(slide){
+    const slider = document.querySelector('.slider');
+
+    slider.innerHTML = `
+        <img src="images/${slide.image}" class="slide-image">
+        <button class="scroll-left">
+            <
+        </button>
+        <button class="scroll-right">
+            >
+        </button>
+    `;
+    const scrollLeft = document.querySelector('.scroll-left');
+
+    scrollLeft.addEventListener('click', () => {
+        index--;
+        if(index < 0){
+            index = slides.length-1;
+        }
+        displaySlide(slides[index]);
+    });
+
+    const scrollRight = document.querySelector('.scroll-right');
+
+    scrollRight.addEventListener('click', () => {
+        index++;
+        if(index >= slides.length){
+            index = 0;
+        }
+        displaySlide(slides[index]);
+    });
+}
