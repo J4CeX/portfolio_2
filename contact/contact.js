15f102e3eb1ce4abb29b853cebfc2daeaa1f57a8
Jacek <jacek_kozakowski_dev@wp.pl>
2023-11-19 14:58:06 +0100

    Send message v3

--- contact/contact.js
+++ contact/contact.js
@@ -1,12 +1,12 @@
 function sendEmail () {
   Email.send({
-    SecureToken: "a500802f-b71c-49b8-93ba-3be360209614",
+    SecureToken: "14091525-f50f-483c-b496-9b849f19b8e1",
     To : 'jacek_kozakowski_dev@wp.pl',
     From : document.getElementById('e-mail').value,
     Subject : "Nowa wiadomosc",
     Body : "Imie i nazwisko: " + document.getElementById('full-name').value 
-    + "\nE-mail: " + document.getElementById('e-mail').value 
-    + "\nWiadomosc: " + document.getElementById('message').value
+    + "<br>E-mail: " + document.getElementById('e-mail').value 
+    + "<br>Wiadomosc: " + document.getElementById('message').value
   }).then(
     message => alert("Wiadomość została poprawnie wysłana")
   );
